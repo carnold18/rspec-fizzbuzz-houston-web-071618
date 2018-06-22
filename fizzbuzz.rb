4846e1dc75c3a44c05eac860fab173bb4741c090
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-22 03:58:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,27 +12,3 @@ def fizzbuzz(int)
   end
 end
 
-
-def fizzbuzz(int)
-  if int % 3 == 0 
-    return "Fizz"
-  elsif int % 5 == 0
-    return "Buzz"
-  elsif int % 15 == 0
-    return "FizzBuzz"
-  else
-    return nil
-  end
-end
-
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 != 0 
-    return "Fizz"
-  elsif int % 5 == 0 && int % 3 != 0
-    return "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    return "FizzBuzz"
-  else
-    return nil
-  end
-end
